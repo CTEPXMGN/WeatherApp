9c495020bbe40d01161760a6727814dd3023a3a1
CTEPXMGN <98664376+CTEPXMGN@users.noreply.github.com>
2022-03-12 19:11:21 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let cityName = '';
 const serverUrl = 'https://api.openweathermap.org/data/2.5/weather';
-const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+const apiKey = '1041b355b3b6422eb66d9f5e517f7b52';
 
 const findBtn = document.querySelector('.find__btn');
 const inputCity = document.querySelector('.input');
@@ -11,6 +11,12 @@ const outputIcon = document.querySelector('.output__icon');
 const addLike = document.querySelector('.like__btn');
 const deleteButton = document.querySelectorAll('.delete__btn');
 const findedCity = document.querySelectorAll('.finded__city');
+const outDetailsCity = document.querySelector('.output__city__details');
+const outDetailsTemp = document.querySelector('.output__details__temperature');
+const outDetailsTemp2 = document.querySelector('.output__details__temperature2');
+const outDetailsWeather = document.querySelector('.output__details__weather');
+const outDetailsSunrise = document.querySelector('.output__details__sunrise');
+const outDetailsSunset = document.querySelector('.output__details__sunset');
 
 async function findCity() {
     if (inputCity.value !== "") {
@@ -37,6 +43,31 @@ async function findCity() {
     }
 };
 
+async function getWeather(){
+    if (inputCity.value !== "") {
+        cityName = inputCity.value;
+        const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+
+        let response = await fetch(url);
+        if (response.ok) {
+            let json = await response.json();
+            outDetailsCity.innerHTML = cityName;
+            console.log(json);
+            let temperature = Math.floor(json.main.temp);
+            outDetailsTemp.innerHTML = 'Temperature: ' + temperature + `°`; 
+
+            let temperature2 = Math.floor(json.main.feels_like);
+            outDetailsTemp2.innerHTML = `Feels like: ` + temperature2 + `°`;
+
+            let weather = json.weather[0].main;
+            outDetailsWeather.innerHTML = `Weather: ` + weather;
+
+            let sunrise = 
+
+        }
+    }
+};
+
 function addCity() {
     if (inputCity.value !== '') {
         document.getElementById('ul').insertAdjacentHTML('afterbegin', `
@@ -44,7 +75,7 @@ function addCity() {
         `);
         const elementID = document.getElementById('ul');
         const firstChild = elementID.firstElementChild;
-        
+
         const btnDelete = firstChild.querySelector('.delete__btn');
         btnDelete.addEventListener('click', function() {
             this.parentElement.remove();
@@ -62,6 +93,7 @@ function addCity() {
 }
 
 findBtn.addEventListener('click', findCity);
+findBtn.addEventListener('click', getWeather);
 addLike.addEventListener('click', addCity);
 
 deleteButton.forEach(function(item) {
